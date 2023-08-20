c01e657eca3c987c98853ec36310d2fc7fe8be30
NguyenHoangHieu22GIT2 <133379564+NguyenHoangHieu22GIT2@users.noreply.github.com>
2023-08-20 16:43:23 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,7 +23,7 @@ import { ChatBoxes } from "@/components/chat/ChatBoxes";
 let socket: Socket;
 
 if (typeof window !== "undefined") {
-  socket = io("https://backend-nguyenhoanghieu1.vercel.app/", {
+  socket = io(process.env.NEXT_PUBLIC_SERVER_URI!, {
     query: {
       userId: sessionStorage.getItem("userId"),
     },
